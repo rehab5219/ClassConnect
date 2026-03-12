0ca5a36a6ebb1c8005891721e29fefac96dce719
Rehab <rehabkamal169@gmail.com>
2026-03-12 23:10:21 +0200

    Some changes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,14 +6,20 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:classconnect/my_app.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(
+      EasyLocalization(
+        supportedLocales: [Locale('en', 'US')],
+        path: 'assets/translations',
+        child: MyApp(),
+      ),
+    );
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
